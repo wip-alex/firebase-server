f02d43568fa0f069f7e222c89d7c22d866fb731b
alex Davis <alex@alex2006hw.com>
2017-09-24 02:43:20 -0700

    module-api

--- functions/index.js
+++ functions/index.js
@@ -6,19 +6,16 @@ var setupGraphQLServer = require('./graphql/server');
 
 /* CF for Firebase with graphql-server-express */
 var graphQLServer = setupGraphQLServer();
-
-// https://us-central1-<project-name>.cloudfunctions.net/api
-// exports.api = functions.https.onRequest(graphQLServer)
-
-
 var dev = process.env.NODE_ENV === 'development';
 var app = next({ dev: dev, conf: { distDir: 'next' } });
 var handle = app.getRequestHandler();
 
+// https://us-central1-<project-name>.cloudfunctions.net/api
+exports.api = functions.https.onRequest(graphQLServer);
+
 exports.next = functions.https.onRequest(function (req, res) {
   console.log('next: ' + req.originalUrl); // log the page.js file that is being requested
   return app.prepare().then(function () {
-    if (req.originalUrl === '/api/graphql') graphQLServer;
     handle(req, res);
   });
 });
--- functionsES6/index.js
+++ functionsES6/index.js
@@ -4,19 +4,16 @@ const setupGraphQLServer = require('./graphql/server')
 
 /* CF for Firebase with graphql-server-express */
 const graphQLServer = setupGraphQLServer()
+const dev = process.env.NODE_ENV === 'development'
+const app = next({ dev, conf: { distDir: 'next' } })
+const handle = app.getRequestHandler()
 
 // https://us-central1-<project-name>.cloudfunctions.net/api
-// exports.api = functions.https.onRequest(graphQLServer)
-
-
-var dev = process.env.NODE_ENV === 'development'
-var app = next({ dev, conf: { distDir: 'next' } })
-var handle = app.getRequestHandler()
+exports.api = functions.https.onRequest(graphQLServer)
 
 exports.next = functions.https.onRequest((req, res) => {
   console.log('next: ' + req.originalUrl) // log the page.js file that is being requested
   return app.prepare().then(() => {
-    if(req.originalUrl === '/api/graphql') graphQLServer;
     handle(req, res)
   })
 })
