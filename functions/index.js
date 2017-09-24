f8651310c2e501e974f9fb1aeb74d009e0c06796
alex Davis <alex@alex2006hw.com>
2017-09-24 00:38:14 -0700

    api-next

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,11 @@ var app = next({ dev, conf: { distDir: 'next' } })
 var handle = app.getRequestHandler()
 
 exports.next = functions.https.onRequest((req, res) => {
-  console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
+  console.log('next: ' + req.originalUrl) // log the page.js file that is being requested
+  return app.prepare().then(() => handle(req, res))
+})
+
+exports.api = functions.https.onRequest((req, res) => {
+  console.log('api: ' + req.originalUrl) // log the page.js file that is being requested
   return app.prepare().then(() => handle(req, res))
 })
